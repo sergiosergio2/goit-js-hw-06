87ea6cd3fc79dfbc9115b9c0381f9f6e33ecacb3
sergiosergio2 <84338029+sergiosergio2@users.noreply.github.com>
2021-11-08 19:00:42 +0200

    task 09 finish

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const button = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const color = document.querySelector('.color');
+
+function getBodyColor() {
+body.setAttribute('style', `background-color: ${getRandomHexColor()}`);
+color.textContent = getRandomHexColor();
+
+}
+button.addEventListener('click', getBodyColor);
+
