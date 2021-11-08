ecb6d746f619eed30d7b88a3ae90d3ffdbade780
sergiosergio2 <84338029+sergiosergio2@users.noreply.github.com>
2021-11-08 16:33:42 +0200

    task 08 finish

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit',onFormSubmit);
+function onFormSubmit (event) {
+    event.preventDefault();
+    const {
+        elements: {email, password}
+    } = event.currentTarget;
 
+    if (email.value === "" || password.value === "") {
+        alert(`Заполните все поля формы!!!`);
+        return;
+    }
+
+    console.log(email.value, password.value);
+    formEl.reset();
+}
