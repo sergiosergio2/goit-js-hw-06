833b2c4f29cba3140bcd2a7f1c4b24780db234cb
sergiosergio2 <84338029+sergiosergio2@users.noreply.github.com>
2021-11-08 22:29:02 +0200

    add task 06

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,26 @@
 const input = document.querySelector('#validation-input');
 input.addEventListener('blur', onCheckLength);
+
 function onCheckLength() {
-   
-    if (input.value.length !== +input.dataset.length ) {
-      return input.classList.add('invalid');
-    }
-    else{
-       return input.classList.replace('invalid', 'valid');
-    }
-    
+  ensureNoValidation(input);
+  if (input.value.length !== +input.dataset.length) {
+    invalid(input);
+    return;
+  }
+    valid(input);
 }
 
+function invalid(input){
+  input.classList.add('invalid');
+  return;    
+}
+function valid(input){
+  input.classList.add('valid');
+  return;
+}
+function ensureNoValidation(input){
+  if(input.classList.contains('invalid', 'valid'))
+    input.classList.remove('invalid');
+    input.classList.remove('valid');
+  
+}
