c2f0cae7349161a9271a0a09d8b1e361c1a2d6b4
sergiosergio2 <84338029+sergiosergio2@users.noreply.github.com>
2021-11-09 11:10:45 +0200

    fixed task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,6 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-const list = document.querySelector("#ingredients");
-for(let ing of ingredients)
-  list.innerHTML+=`<li>${ing}</li>`;
+
+pushToElement("#ingredients", generateElements(ingredients))
+
+
+function generateElements(ingredients) {
+  const newLis = [];
+  for(let ing of ingredients) {
+    let newLi = document.createElement("li");//.innerHTML+=`<li>${ing}</li>`;
+    newLi.textContent = ing;
+    newLi.classList.add("item");
+    newLis.push(newLi);
+  }
+  return newLis;
+}
+
+function pushToElement(element, array) {
+  const list = document.querySelector(element);
+  list.append(...array);
+  
+}
